cb85237f7ac66e86afbf045ad5efedbd7deed402
lilapell <pelll239@newschool.edu>
2020-11-27 17:44:30 -0500

    week11

---
+++ class/main.js
@@ -0,0 +1,23 @@
+function StoryTime(The first) {
+	message = the first + 'time I drank soda'
+	return message
+}
+
+StoryTime('Soda')
+
+let soda = document.querySelector("#soda")
+if(soda.innerHTML=='1'){
+	soda.innerHTML= 'time I drank soda'
+}
+
+>>Output: 1, 2, 3, 4
+
+function Storytime(story) {
+message = 'The first' + story
+console.log (message)
+return message
+}
+
+myFunction(time I drank soda)
+
+>> Output: The first time I drank soda...
