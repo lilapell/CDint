c28d4af63c10d349bc8a624b449e190102fb0841
lilapell <pelll239@newschool.edu>
2020-12-04 08:38:39 -0500

    interactive

---
+++ Interactive/main.js
@@ -0,0 +1,15 @@
+let selection = 0
+
+function update() {
+	let options = document.querySelectorAll('.option')
+	let title = document.querySelector('h1')
+	console.log(options)
+	options.forEach(item => item.onclick=function() {
+		selection = item.id
+		console.log(selection)
+		title.innerHTML= "I " + selection + " the blade."
+	})
+
+}
+
+update();
